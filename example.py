6ab9ada09cd58196010a7d0596d3675ae416aadd
Razor <kaifj99@gmail.comgit config --global user.email kaifj99@gmail.com>
2016-01-29 20:24:19 +0530

    the simple program done

--- example.py
+++ example.py
@@ -1,6 +1,11 @@
+from random import randint
 import time
-start = time.time()
-12/6
-end= time.time()
-elapsed = start-end
-print elapsed
+avg=0
+i=0
+while i < 100:
+    start= time.time()
+    randint(0,100) + randint(0,100)
+    end = time.time()
+    avg = avg + (end -start)
+    i += 1
+print "The average time taken by my processor to perform an add is %s seconds " %avg   
